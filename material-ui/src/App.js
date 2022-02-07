ecf1cf524811a3b46ecf7fe902e5843eb2d59703
Yasikhorram <khorramyasaman@gmail.com>
2022-02-07 11:34:12 -0600

    Add groupButton

--- material-ui/src/App.js
+++ material-ui/src/App.js
@@ -2,20 +2,33 @@ import logo from "./logo.svg";
 import "./App.css";
 import Button from "@mui/material/Button";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import CancelPresentationOutlinedIcon from "@mui/icons-material/CancelPresentationOutlined";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button
-          startIcon={<AddShoppingCartIcon />}
-          size="large"
-          onClick={() => alert("Hello! You just clicked.")}
-          variant="contained"
-          color="primary"
-        >
-          Testing MUI Button
-        </Button>
+        <ButtonGroup>
+          <Button
+            startIcon={<AddShoppingCartIcon />}
+            size="large"
+            onClick={() => alert("Added to shopping card.")}
+            variant="contained"
+            color="primary"
+          >
+            Testing MUI Button
+          </Button>
+          <Button
+            startIcon={<CancelPresentationOutlinedIcon />}
+            size="large"
+            onClick={() => alert("Removed from shopping card")}
+            variant="contained"
+            color="secondary"
+          >
+            Testing MUI Button
+          </Button>
+        </ButtonGroup>
 
         <img src={logo} className="App-logo" alt="logo" />
       </header>
