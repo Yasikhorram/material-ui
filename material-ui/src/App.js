aab501c26972faef3a0ca1df9e44f1dd77998553
Yasikhorram <khorramyasaman@gmail.com>
2022-02-07 10:55:45 -0600

    Add icon

--- material-ui/src/App.js
+++ material-ui/src/App.js
@@ -1,14 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Button from "@mui/material/Button";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button variant="contained" color="primary">
+        <Button
+          startIcon={<AddShoppingCartIcon />}
+          size="large"
+          onClick={() => alert("Hello! You just clicked.")}
+          variant="contained"
+          color="primary"
+        >
           Testing MUI Button
         </Button>
+
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
@@ -16,3 +24,5 @@ function App() {
 }
 
 export default App;
+
+// #6200EE
